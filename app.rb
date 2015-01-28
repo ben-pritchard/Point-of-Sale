7745ae4ab595d38ce78e4abcb03209c78cbf6e74
Ben Pritchard & Marc Lee <student@epicodus.com>
2015-01-27 16:57:05 -0800

    Add sinatra functionality, stuck on purchases button in table with check boxes.

--- app.rb
+++ app.rb
@@ -13,6 +13,43 @@ get('/') do
   erb(:index)
 end
 
-get("/product/:product_id") do
-  
+get("/product/:id") do
+  @product = Product.find(params.fetch("id").to_i())
+  erb(:product)
+end
+
+post("/add") do
+  name = params.fetch("name")
+  price = params.fetch("price").to_d()
+  product = Product.create({:name => name, :price => price})
+  redirect("/")
+end
+
+patch("/edit_name") do
+  name = params.fetch("name")
+  @product = Product.find(params.fetch("id").to_i())
+  @product.update({:name => name})
+  erb(:product)
+end
+
+patch("/edit_price") do
+  price = params.fetch("price")
+  @product = Product.find(params.fetch("id").to_i())
+  @product.update({:price => price})
+  erb(:product)
+end
+
+post("/delete_product") do
+  @product = Product.find(params.fetch("id").to_i())
+  @product.destroy()
+  redirect("/")
+end
+
+post("/make_purchase") do
+  product_id_array = params.fetch("product_ids")
+  @purchase = Purchase.create({:name => "Purchase"})
+  product_id_array.each() do |product_id|
+    Product.find(product_id).update({:purchase_id => @purchase.id()})
+  end
+  erb(:purchase)
 end
--- spec/purchase_spec.rb
+++ spec/purchase_spec.rb
@@ -2,8 +2,9 @@ require('spec_helper')
 
 describe(Purchase) do
   it("tells which products associate with the purchase") do
-  purchase = Purchase.create({:name => "Jason"})
-  product1 = Product.create({:name => "hockey mask", :price => 150, :purchase_id => purchase.id()})
-  product2 = Product.create({:name => "large kitchen knife", :price => 350, :purchase_id => purchase.id()})
+    purchase = Purchase.create({:name => "Jason"})
+    product1 = Product.create({:name => "hockey mask", :price => 150, :purchase_id => purchase.id()})
+    product2 = Product.create({:name => "large kitchen knife", :price => 350, :purchase_id => purchase.id()})
+    expect(purchase.products()).to(eq([product1, product2]))
   end
 end
